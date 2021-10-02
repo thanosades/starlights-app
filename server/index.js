1818f9e8218ec1b705dd1bac4ce731a2d4fe9019
Thanos Dimitriades <thanosades@gmail.com>
2021-10-02 18:36:49 +0300

    basic server setup with mongo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+/* eslint-disable no-console */
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+const app = express();
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
+
+const parserOptions = { limit: "30mb", extended: true };
+app.use(express.json(parserOptions));
+app.use(express.urlencoded(parserOptions));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://" + process.env.DB_USER + ":"  + process.env.DB_PASS +
+  "@cluster0.hozzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}.`)))
+  .catch(() => (error) => console.error(error.message));
