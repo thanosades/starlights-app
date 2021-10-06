7408cc672e46027c1a423a5f261626cdc34d695b
Thanos Dimitriades <thanosades@gmail.com>
2021-10-06 22:34:43 +0300

    add comment for clarification

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -16,6 +17,24 @@ export const createPost = async (req, res) => {
   try {
     await newPost.save();
     res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+}
+
+export const updatePost = async (req, res) => {
+  let { id } = req.params;
+  // needed to avoid false positives like in 12 chars long strings
+  const isObjectIdValid = ObjectId.isValid(id) && String(new ObjectId(id) === id);
+  const post = req.body;
+
+  if (!isObjectIdValid || !post) {
+    res.status(400).json({ message: 'Malformed patch document' });
+  }
+
+  try {
+    const updatedPost = PostMessage.findByIdAndUpdate(id, post, { new: true });
+    res.json(updatedPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
