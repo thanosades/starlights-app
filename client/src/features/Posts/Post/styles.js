e1fc3d2155cfda84473340243f8cc726c0cd6d4a
Thanos Dimitriades <thanosades@gmail.com>
2021-11-17 15:50:23 +0200

    change hover style

--- client/src/features/Posts/Post/styles.js
+++ client/src/features/Posts/Post/styles.js
@@ -6,6 +6,9 @@ export default makeStyles({
     paddingTop: '56.25%',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     backgroundBlendMode: 'darken',
+    '&:hover': {
+      backgroundBlendMode: 'normal'
+    }
   },
   border: {
     border: 'solid',
